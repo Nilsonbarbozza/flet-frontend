125d0aa5cef857f1630ed9adeaababe24acd0f72
Nilson Barboza <barbosa.nilsonneto@gmail.com>
2025-02-20 10:47:21 -0300

    Inicio de style

--- app.py
+++ app.py
@@ -4,16 +4,21 @@ import requests
 API_BASE_URL = "http://localhost:8000/api"
 
 def main(page: ft.Page):
-    page.title = "PageTeste"
+    page.title = "Area de Cadastro"
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.bgcolor = ft.colors.WHITE
+    page.margin= ft.margin.only(left=16, top=20, right=16, bottom=20)
+
+
 
 
     #Campo da aplicaçã
-    nome_field = ft.TextField(label="Nome")
-    email_field = ft.TextField(label="email")
-    faixa_field = ft.TextField(label="faixa")
-    data_nascimento_field = ft.TextField(label="Data nascimento (yyyy-mm-aaa)")
+    nome_field = ft.TextField(label="Nome", label_style= ft.TextStyle(size=12, color="black"), border_radius=10)
+    email_field = ft.TextField(label="email",label_style= ft.TextStyle(size=12, color="black"), border_radius=10)
+    faixa_field = ft.TextField(label="faixa",label_style= ft.TextStyle(size=12, color="black"), border_radius=10)
+    data_nascimento_field = ft.TextField(label="Data nascimento (yyyy-mm-aaa)",label_style= ft.TextStyle(size=12, color="black"), border_radius=10)
     create_result = ft.Text()
+    container = ft.Container(height=8)
 
     #Criar a função de orientação a evento
     def criar_aluno_click(e):
@@ -33,19 +38,25 @@ def main(page: ft.Page):
         else:
             create_result.value = f'Error ao criar aluno{response.text}'
         
+        
         page.update()
 
-    create_button = ft.ElevatedButton(text="Cadastrar", on_click=criar_aluno_click) 
+    create_button = ft.ElevatedButton(text="Enviar", height=40, width=100, bgcolor=ft.colors.BLUE_600, color="white",style=ft.ButtonStyle(
+        shape=ft.RoundedRectangleBorder(radius=12),
+        padding=ft.padding.symmetric(horizontal=20, vertical=14),
+        overlay_color=ft.colors.BLUE_600,
+        elevation=4,), on_click=criar_aluno_click) 
 
     #Cria uma page Container colunas
     criar_tabela_aluno = ft.Column(
-        [
+        [   container,
             nome_field,
             email_field,
             faixa_field,
             data_nascimento_field,
-            create_result,
-            create_button
+            create_button,
+            create_result
+            
         ], scroll=True
     )
 
@@ -86,15 +97,20 @@ def main(page: ft.Page):
             list_result.value = f"{len(alunos)} alunos encontrados"
             list_result.color = "green"
             page.update()
-
+    container = ft.Container(height=8)
     list_result = ft.Text()
-    list_button = ft.ElevatedButton(text="Listar Alunos", on_click=listar_alunos_click )
-    listar_alunos_tab = ft.Column([students_table, list_result, list_button], scroll=True)
+    list_button = ft.ElevatedButton(text="Listar Alunos", height=40, width=200, bgcolor=ft.colors.BLUE_600, color="white",style=ft.ButtonStyle(
+        shape=ft.RoundedRectangleBorder(radius=12),
+        padding=ft.padding.symmetric(horizontal=20, vertical=14),
+        overlay_color=ft.colors.BLUE_600,
+        elevation=4,), on_click=listar_alunos_click )
+    listar_alunos_tab = ft.Column([container, students_table, list_button, list_result], scroll=True)
 
     #Registrar aula
-    email_aula_field = ft.TextField(label="Email Aluno")
-    qtd_field = ft.TextField(label="Quantidade de aula", value=1)
+    email_aula_field = ft.TextField(label="Email Aluno",label_style= ft.TextStyle(size=12, color="black"), border_radius=10)
+    qtd_field = ft.TextField(label="Quantidade de aula", value=1,label_style= ft.TextStyle(size=12, color="black"), border_radius=10)
     aula_result = ft.Text()
+    container = ft.Container(height=8)
 
     def marca_aula(e):
         payload = {
@@ -111,16 +127,21 @@ def main(page: ft.Page):
         
         page.update()
 
-    aula_button = ft.ElevatedButton(text="Registrar", on_click=marca_aula)
-    aula_tab = ft.Column([email_aula_field, qtd_field, aula_result, aula_button], scroll=True)
+    aula_button = ft.ElevatedButton(text="Registrar", height=40, width=100, bgcolor=ft.colors.BLUE_600, color="white",style=ft.ButtonStyle(
+        shape=ft.RoundedRectangleBorder(radius=12),
+        padding=ft.padding.symmetric(horizontal=20, vertical=14),
+        overlay_color=ft.colors.BLUE_600,
+        elevation=4,), on_click=marca_aula)
+    aula_tab = ft.Column([container, email_aula_field, qtd_field, aula_button,aula_result], scroll=True)
 
     #Progresso aluno
-    email_progress_field = ft.TextField(label="Email-Aluno")
+    email_progress_field = ft.TextField(label="Email-Aluno",label_style= ft.TextStyle(size=12, color="black"), border_radius=10)
     progress_aluno_field = ft.Text()
 
     
 
     def progress_click(e):
+        container = ft.Container(height=8)
         email = email_progress_field.value
         response = requests.get(API_BASE_URL + '/progresso_aluno/', params={'email_aluno':email})
 
@@ -139,15 +160,19 @@ def main(page: ft.Page):
 
         page.update()
 
-    progress_button = ft.ElevatedButton(text="Ver progresso", on_click=progress_click)
-    progress_tab = ft.Column([email_progress_field,progress_aluno_field,progress_button ], scroll=True)
+    progress_button = ft.ElevatedButton(text="Ver progresso", height=40, width=200, bgcolor=ft.colors.BLUE_600, color="white",style=ft.ButtonStyle(
+        shape=ft.RoundedRectangleBorder(radius=12),
+        padding=ft.padding.symmetric(horizontal=20, vertical=14),
+        overlay_color=ft.colors.BLUE_600,
+        elevation=4,), on_click=progress_click)
+    progress_tab = ft.Column([container, email_progress_field,progress_button, progress_aluno_field ], scroll=True)
 
-
-    id_aluno_field = ft.TextField(label="ID do Aluno")
-    nome_update_field = ft.TextField(label="Novo Nome")
-    email_update_field = ft.TextField(label="Novo Email")
-    faixa_update_field = ft.TextField(label="Nova Faixa")
-    data_nascimento_update_field = ft.TextField(label="Nova Data de Nascimento (YYYY-MM-DD)")
+    container = ft.Container(height=8)
+    id_aluno_field = ft.TextField(label="ID do Aluno",label_style= ft.TextStyle(size=12, color="black"), border_radius=10)
+    nome_update_field = ft.TextField(label="Novo Nome",label_style= ft.TextStyle(size=12, color="black"), border_radius=10)
+    email_update_field = ft.TextField(label="Novo Email",label_style= ft.TextStyle(size=12, color="black"), border_radius=10)
+    faixa_update_field = ft.TextField(label="Nova Faixa",label_style= ft.TextStyle(size=12, color="black"), border_radius=10)
+    data_nascimento_update_field = ft.TextField(label="Nova Data de Nascimento (YYYY-MM-DD)",label_style= ft.TextStyle(size=12, color="black"), border_radius=10)
     update_result = ft.Text()
 
     def atualizar_aluno_click(e):
@@ -177,9 +202,13 @@ def main(page: ft.Page):
 
         page.update()
 
-    update_button = ft.ElevatedButton(text="Atualizar Aluno", on_click=atualizar_aluno_click)
+    update_button = ft.ElevatedButton(text="Atualizar Aluno", height=40, width=200, bgcolor=ft.colors.BLUE_600, color="white",style=ft.ButtonStyle(
+        shape=ft.RoundedRectangleBorder(radius=12),
+        padding=ft.padding.symmetric(horizontal=20, vertical=14),
+        overlay_color=ft.colors.BLUE_600,
+        elevation=4,), on_click=atualizar_aluno_click)
     atualizar_tab = ft.Column(
-        [
+        [   container,
             id_aluno_field,
             nome_update_field,
             email_update_field,
@@ -193,8 +222,9 @@ def main(page: ft.Page):
 
 
     # Deletar usuário
-    id_aluno_field = ft.TextField(label="ID do aluno a deletar")
-    email_aluno_field = ft.TextField(label="E-mail")
+    container = ft.Container(height=8)
+    id_aluno_field = ft.TextField(label="ID do aluno a deletar",label_style= ft.TextStyle(size=12, color="black"), border_radius=10)
+    email_aluno_field = ft.TextField(label="E-mail",label_style= ft.TextStyle(size=12, color="black"), border_radius=10)
     result_delet = ft.Text(color="red")  # Exibir erros em vermelho
 
     def delete_aluno(e):
@@ -227,8 +257,12 @@ def main(page: ft.Page):
 
         page.update()
 
-    delet_button = ft.ElevatedButton(text="Deletar", on_click=delete_aluno)
-    delet_tab = ft.Column([id_aluno_field, email_aluno_field, result_delet, delet_button], scroll=True)
+    delet_button = ft.ElevatedButton(text="Deletar", height=40, width=100, bgcolor=ft.colors.RED, color="white",style=ft.ButtonStyle(
+        shape=ft.RoundedRectangleBorder(radius=12),
+        padding=ft.padding.symmetric(horizontal=20, vertical=14),
+        overlay_color=ft.colors.BLUE_600,
+        elevation=4,), on_click=delete_aluno)
+    delet_tab = ft.Column([container, id_aluno_field, email_aluno_field, delet_button,result_delet], scroll=True)
     
 
 
@@ -236,16 +270,21 @@ def main(page: ft.Page):
     tabs = ft.Tabs(
         selected_index = 0,
         animation_duration= 300,
-
+        expand=True,  # Faz com que as tabs ocupem toda a largura
+        indicator_color="blue",  # Cor do indicador da aba selecionada
+        divider_color="transparent",  # Remove a linha divisória
+    
         tabs=[
-            ft.Tab(text="Cadastro", content=criar_tabela_aluno),
-            ft.Tab(text="Alunos", content=listar_alunos_tab),
-            ft.Tab(text="Concluida", content=aula_tab),
-            ft.Tab(text="Progresso", content=progress_tab),
-            ft.Tab(text="Atualizar", content=atualizar_tab),
-            ft.Tab(text="Deletar", content=delet_tab, ),
+            ft.Tab(text="Cadastro", icon=ft.icons.PERSON_ADD, content=criar_tabela_aluno),
+            ft.Tab(text="Alunos", icon=ft.icons.GROUP, content=listar_alunos_tab),
+            ft.Tab(text="Concluida", icon=ft.icons.CHECK_CIRCLE, content=aula_tab),
+            ft.Tab(text="Progresso", icon=ft.icons.TRENDING_UP, content=progress_tab),
+            ft.Tab(text="Atualizar", icon=ft.icons.EDIT, content=atualizar_tab),
+            ft.Tab(text="Deletar", icon=ft.icons.DELETE,  content=delet_tab),
         ]
+
     )
+    
 
     page.add(tabs)
 
